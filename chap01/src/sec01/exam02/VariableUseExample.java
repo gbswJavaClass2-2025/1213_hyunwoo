ba6cbb50540b0ddebfdab19cbe75842f3b5dbf4f
love09pc <love09pc@naver.com>
2025-08-21 14:41:29 +0900

    asd

---
+++ chap01/src/sec01/exam02/VariableUseExample.java
@@ -0,0 +1,12 @@
+package sec01.exam02;
+
+public class VariableUseExample {
+
+	public static void main(String[] args) {
+		int value = 30;
+		int result = value + 10;
+		System.out.println(result);
+
+	}
+
+}
