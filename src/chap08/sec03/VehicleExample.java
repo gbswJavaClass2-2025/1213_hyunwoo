05a497506a2a893109ec3f36ab6a8d025777a37c
love09pc <love09pc@naver.com>
2025-10-28 11:21:12 +0900

    asd

---
+++ src/chap08/sec03/VehicleExample.java
@@ -0,0 +1,15 @@
+package chap08.sec03;
+
+public class VehicleExample {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Bus();
+
+        vehicle.run();
+//        vehicle.checkFare();
+
+        Bus bus = (Bus) vehicle;
+
+        bus.run();
+        bus.checkFare();
+    }
+}
