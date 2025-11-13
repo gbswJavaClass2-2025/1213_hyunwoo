eb0a4d3e6b083061af6cb8f9ebe1905c3a4bf592
love09pc <love09pc@naver.com>
2025-11-13 12:25:11 +0900

    챕8 끝

---
+++ src/chap08/sec02/exam05/Example.java
@@ -0,0 +1,20 @@
+package chap08.sec02.exam05;
+
+public class Example {
+    public static void main(String[] args) {
+        ImplementationC impl = new ImplementationC();
+
+//        InterfaceA ia = impl;
+//        ia.mathodA();
+//        System.out.println();
+//
+//        InterfaceB ib = impl;
+//        ib.mathodB();
+//        System.out.println();
+//
+//        InterfaceC ic = impl;
+//        ic.methodA();
+//        ic.methodB();
+//        ic.methodC();
+    }
+}
